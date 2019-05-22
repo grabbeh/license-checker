dd4adbcfb6cc0dad7b43e681368a5fd77c1d5e40
Michael <mbg@outlook.com>
2019-05-23 00:25:13 +0100

    Update to add missing licenses

--- src/components/AttributionList.js
+++ src/components/AttributionList.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Box from '../components/Box'
 import Text from '../components/Text'
+import TextArea from './TextArea'
+import Button from './Button'
 
 const AttributionList = ({ dependencies }) => {
+  // map over items and return new array with new content
+  // https://www.robinwieruch.de/react-state-array-add-update-remove/
+  let [deps, setDependencies] = useState(dependencies)
+  let [licenseText, setLicense] = useState()
+  let [editableIndex, setEditableIndex] = useState()
+  let [editableLicenseIndex, setLicenseIndex] = useState()
+
+  useEffect(() => {
+    setDependencies(dependencies)
+  }, [])
+
+  const submitLicense = () => {
+    // TODO: Cleaner way of doing adding license text
+    let dep = deps[editableIndex]
+    let license = dep.licenses[editableLicenseIndex]
+    let revised = (license = {
+      ...license,
+      text: licenseText
+    })
+    let licenses = dep.licenses.map((l, i) => {
+      if (editableLicenseIndex === i) {
+        return revised
+      } else {
+        return {
+          l
+        }
+      }
+    })
+    let revisedDep = {
+      ...dep,
+      licenses
+    }
+    let revisedDeps = deps.map((d, i) => {
+      if (editableIndex === i) {
+        return revisedDep
+      } else return d
+    })
+    setDependencies(revisedDeps)
+    setEditableIndex(null)
+    setLicense(null)
+  }
   return (
     <Box>
       <Box>
@@ -10,18 +53,65 @@ const AttributionList = ({ dependencies }) => {
           License list
         </Text>
       </Box>
-      {dependencies.map(d => {
-        return d.licenses.map((l, i) => (
-          <Box pb={2} borderBottom='1px solid' borderColor='black' key={i}>
-            <Box py={2}>
-              <Text fontWeight='bold'>{d.name}</Text>
+      {deps.map((d, dependencyIndex) => {
+        return d.licenses.map(({ text }, licenseIndex) => {
+          return (
+            <Box
+              pb={2}
+              borderBottom='1px solid'
+              borderColor='black'
+              key={dependencyIndex}
+            >
+              <Box py={2}>
+                <Text fontWeight='bold'>{d.name}</Text>
+              </Box>
+              {text ? (
+                <Text fontSize={2}>
+                  <pre>{text}</pre>
+                </Text>
+              ) : (
+                <Box>
+                  <Text>Sorry, we can't seem to find a license</Text>
+                  <Text
+                    onClick={() => {
+                      setEditableIndex(dependencyIndex)
+                      setLicenseIndex(licenseIndex)
+                    }}
+                  >
+                    Add one
+                  </Text>
+                </Box>
+              )}
+              {!text &&
+                editableIndex === dependencyIndex &&
+                editableLicenseIndex === licenseIndex && (
+                <Box>
+                  <TextArea
+                    border='1px solid'
+                    borderColor='black'
+                    width={1}
+                    handleChange={e => {
+                      setLicense(e.target.value)
+                    }}
+                    value={licenseText}
+                    height={400}
+                    name='licenseText'
+                    placeholder='Enter a license'
+                  />
+                  <Button
+                    onClick={() => {
+                      submitLicense()
+                    }}
+                  >
+                    <Text color='white' fontSize={2}>
+                        Submit
+                    </Text>
+                  </Button>
+                </Box>
+              )}
             </Box>
-
-            <Text fontSize={2}>
-              <pre>{l.text}</pre>
-            </Text>
-          </Box>
-        ))
+          )
+        })
       })}
     </Box>
   )
